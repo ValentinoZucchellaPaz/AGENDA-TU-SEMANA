980f48454bc194476f67bc4f6588254106029ad1
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2022-09-18 17:05:56 -0300

    segundaEntregaProyectoFinal

--- script.js
+++ script.js
@@ -1,171 +1,129 @@
-/**ARRAYS DONDE SE GUARDAN DATOS */
-
 class Task {
-    constructor(hour, name, description, id) {
-        this.hour = hour;
+    constructor (day, name, hour, definition, id){
+        this.day = this.toArray(day);
         this.name = name;
-        this.description = description;
+        this.hour = hour; //this.toHour(hour);
+        this.definition = definition;
         this.id = id;
     }
-}
-
-let lunes = [
-    new Task ('19:00', 'KARATE', 'ir a entrenar', 1),
-];
-let martes = [];
-let miercoles = [
-    new Task ('19:00', 'KARATE', 'ir a entrenar', 2),
-];
-let jueves = [];
-let viernes = [
-    new Task ('19:00', 'KARATE', 'ir a entrenar', 3),
-];
-let sabado = [];
-let domingo = [];
-
-let tasks = [lunes, martes, miercoles, jueves, viernes, sabado, domingo];
-
-
-
-
-/**         MANIPULAR DATOS DE ARRAY         */
-
-/**AGREGAR TAREAS AL ARRAY*/
-function createTask(day, hour, name, description, id) {
-    if (day < 8) {
-        day -= 1;
-        tasks[day].push(new Task(hour, name, description, id));
-        alert('Tu tarea se asignó con el id ' + id);
-    } else if (day == 8) {
-        tasks.forEach((array, counter) => {
-            const newId = `${name}-${counter+1}`;
-            array.push(new Task(hour, name, description, newId));
-        });
+    toArray(day) {
+        day = day.split(', ');
+        return day;
     }
 }
-
-/**ELIMINAR TAREAS DEL ARRAY*/
-function deleteTask(day, Name) {
-    if (day < 8) {
-        
-        day -= 1;
-        
-        let deletedTasks = tasks[day].filter(obj => obj.name == Name);
-        console.log('TAREAS ELIMINADAS DEL DÍA ' + (day+1));
-        console.log(deletedTasks)
-        
-        // ELIMINAR DE ARRAY
-        tasks[day] = tasks[day].filter(obj => obj.name !== Name);
-
-    } else if (day == 8) { //eliminar todas con determinado nombre
-        
-        // output
-        deletedTasks = tasks.map(dayArray => dayArray.filter(obj => obj.name == Name));
-        alert ('LAS TAREAS ELIMINADAS SE MOSTRARÁN POR CONSOLA');
-        console.log('TAREAS ELIMINADAS DE TODOS LOS DÍAS');
-        console.log(deletedTasks)
-        
-        // ELIMINAR DE ARRAY
-        tasks = tasks.map(dayArray => dayArray.filter(obj => obj.name !== Name));
-        
-    }
-}
-
-/**ELIMINAR TODAS LAS TAREAS */
-function deleteAllTasks () {
-    tasks = tasks.map(dayArray => dayArray=[]);
-    alert('Todas las tareas fueron eliminadas');
-}
-
-/**ASIGNAR ID a partir de n° de tareas */
-function asignarId(){
-    return lastId() + 1;
-}
-function lastId () {
-    let allIds = tasks.flatMap(array => array.map(obj => obj.id));
-    return allIds.length;
-}
+let tasks = [
+    new Task('lunes, miercoles, viernes', 'KARATE', '19:00', 'ir a entrenar', 1),
+    new Task('lunes', 'entrenar', '17:00', '', 2),
+    new Task('viernes', 'estudiar', '8:00', 'mate y fisica', 3),
+]
 
 
 
 
-/**                 DOM                  */
-
-
-
-
-/**CREAR TAREA DESDE HTML */
+// toma datos de los input y 1.agrega a html 2.crea tarea
 const createTaskBtn = document.getElementById('submit')
 createTaskBtn.addEventListener('click', () => {
-    const inputName = document.getElementById('task-selector').value.toUpperCase();
+    let inputName = document.getElementById('task-selector').value.toUpperCase();
     const inputDay = document.getElementById('day-selector').value;
     const inputHour = document.getElementById('hour-selector').value;
     const inputDescription = document.getElementById('task-description').value;
-    const newTaskId = asignarId();
+    const id = asingId();
+
     
-    createTask(Number(inputDay), inputHour, inputName, inputDescription, newTaskId);
-
-    // opcion de agregar una tarea todos los días
-    if (inputDay == 8) {
-        for (let i = 1; i < inputDay; i++) {
-            addTaskDOM(i, (newTaskId + (i-1)), inputName, inputHour)
-        }
-    } else if (inputDay < 8) {
-        addTaskDOM(inputDay, newTaskId, inputName, inputHour);
-    }
+    
+    addTaskToDOM(inputDay, inputName, id, inputHour);
+    createTask(inputDay, inputName, inputHour, inputDescription, id);
 });
 
-/**IMPRIMIR TASKS EN HTML (las que ya están guardadas)*/
-tasks.forEach((days, counter) => {
-    days.forEach(task => {
-        addTaskDOM((counter+1), task.id, task.name, task.hour)
-    })
-});
+// agrega al HTML + funciones borrar y checkear cuando click en btn
+function addTaskToDOM(day, taskName, taskId, taskHour) {
+    const ul = document.getElementById(`${day}-ul`);
+    const li = document.createElement('li');
+    const liId = `${day}-${taskName.trim()}-${taskId}`
+    li.setAttribute('id', liId);
+    li.innerHTML = `
+        <p style="display:inline;" >${taskName}: </p> <span style="font-weight:400;">${taskHour} hs.</span> 
+        <div class="btn-container"> 
+            <button class="btn-delete" id="delete-${liId}" title="Eliminar tarea">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+            <button class="btn-check" id="completed-${liId}" title="Tarea completada">
+                <i class="fa-solid fa-check" id="icon-check-${liId}"></i>
+            </button>
+            </div>`;
+    ul.append(li);
+    
+    deleteTaskBtnDOM(ul, li, `delete-${liId}`, taskId, day);
 
-
-/**AGREGAR TAREA AL HTML (junto con botones para borrar y marcar como completa) */
-function addTaskDOM(dayNumber, taskId, taskName, taskHour) {
-    const UlSelector = document.getElementById(`day${dayNumber}-ul`);
-    const taskLi = document.createElement('li');
-    taskLi.setAttribute('id', `task-${taskId}`);
-    taskLi.innerHTML = `
-    <p style="display:inline;" >${taskName}: </p> <span style="font-weight:400;">${taskHour} hs.</span> 
-    <div class="btn-container"> 
-        <button class="btn-delete" id="delete-${taskId}" title="Eliminar tarea">
-            <i class="fa-solid fa-xmark"></i>
-        </button>
-        <button class="btn-check" id="completed-${taskId}" title="Tarea completada">
-            <i class="fa-solid fa-check" id="icon-check-${taskId}"></i>
-        </button>
-    </div>`;
-    UlSelector.append(taskLi);
-
-    deleteTaskDOM(UlSelector, taskLi, `delete-${taskId}`, dayNumber, taskName);
-
-    completedTaskDOM(taskLi, `completed-${taskId}`, `icon-check-${taskId}`);
+    completeTaskBtnDOM(li, `completed-${liId}`, `icon-check-${liId}`);
 }
 
-/**MARCAR COMO COMPLETA DESDE BOTON */
-function completedTaskDOM (liHtml, btnId, iconId){
-    const confirmBtn = document.getElementById(btnId);
+// cuando se presiona btn (btnId), html remueve child del father, saca dayToDelete de task (taskId)
+function deleteTaskBtnDOM(father, child, btnId, taskId, dayToDelete) {
+    const deleteBtn = document.getElementById(btnId);
+    deleteBtn.addEventListener('click', ()=> {
+        father.removeChild(child);
+        deleteDayFromTask(taskId, dayToDelete);
+    });
+}
+
+// elimina dayToDelete de task (taskId), actualiza LS
+function deleteDayFromTask (taskId, dayToDelete) {
+    let taskToDeleteDay = tasks.filter(task => task.id == taskId);
+    taskToDeleteDay = taskToDeleteDay.shift();
+    let taskDays = taskToDeleteDay.day;
+    taskDays = taskDays.filter(day => day != dayToDelete)
+    taskToDeleteDay.day = taskDays;
+    
+    if (taskDays.length == 0) {
+        deleteTask(taskId)
+    } else if (taskDays.length > 0) {
+        deleteTask(taskId);
+        console.log(`SOLO DEL DÍA ${dayToDelete}`);
+        tasks.push(taskToDeleteDay);
+    }
+    refreshLS();
+}
+
+// elimina task (taskId) de tasks
+function deleteTask (taskId) { 
+    const deletedTask = tasks.filter(task => task.id == taskId);
+    tasks = tasks.filter(task => task.id != taskId);
+    console.log(`Se eliminó la tarea: ${deletedTask[0].name}`);
+    console.log(deletedTask);
+}
+
+
+// cuando presiono btn (btnId), se muestra verde li e icon (iconId)
+function completeTaskBtnDOM (li, btnId, iconId){
+    const checkBtn = document.getElementById(btnId);
     const checkIcon = document.getElementById(iconId);
     
-    confirmBtn.addEventListener('click', ()=> {
-        liHtml.classList.toggle('completed');
+    checkBtn.addEventListener('click', ()=> {
+        li.classList.toggle('completed');
         checkIcon.classList.toggle('completed');
     })
 }
 
-/**ELIMINAR TAREA DESDE BOTON */
-function deleteTaskDOM(fatherHTML, taskHTML, btnIdHTML, dayPosition, taskName) {
-    const deleteBtn = document.getElementById(btnIdHTML);
-    deleteBtn.addEventListener('click', ()=> {
-        fatherHTML.removeChild(taskHTML)
-        deleteTask(dayPosition, taskName);
-    });
+// crea tarea y sube a tasks, actualiza LS
+function createTask (day, name, hour, descripction, id){
+    let newTask = new Task (day, name, hour, descripction, id)
+    tasks.push(newTask);
+    refreshLS();
+}
+function asingId () {
+    return tasks.length + 1;
 }
 
-/**BOTON ELIMINAR TODAS LAS TAREAS */
+
+
+
+
+
+
+
+
+// elimina todas tareas, actualiza LS
 const deleteAllTasksBtn = document.getElementById('delete-all');
 deleteAllTasksBtn.addEventListener('click', ()=>{
     let condition = prompt('¿Estás seguro que quieres borrar todas las tareas?\n1. Sí    2. No').toLowerCase();
@@ -173,7 +131,38 @@ deleteAllTasksBtn.addEventListener('click', ()=>{
         const allUlSelector = document.querySelectorAll('.day-ul');
         allUlSelector.forEach(ul => ul.innerHTML = null);
 
-
-        deleteAllTasks();
+        tasks = [];
+        refreshLS();
     }
 })
+
+
+
+
+
+
+// LOCALSTORAGE
+if (localStorage.length == 0) { //inicializa
+    addToLS(tasks, 'allTasks');
+}
+// agrega array a LS
+function addToLS (array, localStorageName) {
+    const arrayJSON = JSON.stringify(array);
+    localStorage.setItem(localStorageName, arrayJSON);
+}
+// borra LS y agrega tasks
+function refreshLS() { //borra LS y agrega tasks
+    localStorage.clear();
+    addToLS(tasks, 'allTasks');
+}
+
+// PRINT IN DOM FROM LOCALSTORAGE (when charge page)
+const allTasksLS = localStorage.getItem('allTasks');
+tasks = [];
+const allTasksLSArray = JSON.parse(allTasksLS);
+tasks = allTasksLSArray;
+allTasksLSArray.forEach(task => {
+    task.day.forEach(element => {
+        addTaskToDOM(element, task.name, task.id, task.hour);
+    })
+});
