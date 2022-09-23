2db8eaa6e914c191859004ca29af212fed9f1d5a
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2022-09-23 12:08:06 -0300

    operadoresAvanzados-librerías-funcionCompletarTarea

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 class Task {
     constructor (day, name, hour,  id){
-        this.day = this.toArray(day);
+        this.day = day;
         this.name = name;
         this.hour = hour;
         this.id = id;
+        this.completed = []
     }
     toArray(day) {
         day = day.split(', ');
@@ -16,13 +17,14 @@ let tasks = [
     new Task('viernes', 'estudiar', '8:00', 3),
 ]
 
-// TODO: que se guarde estado de tarea y se actualice cuando presione boton, agregar funcion de editar tarea
+// TODO: funcion de editar tarea
 
 
 
 // toma datos de los input y 1.agrega a html 2.crea tarea
 const createTaskBtn = document.getElementById('submit');
-createTaskBtn.addEventListener('click', () => {
+createTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     let inputName = document.getElementById('task-selector').value.toUpperCase();
     let inputHour = document.getElementById('hour-selector').value;
     const id = asingId();
@@ -36,18 +38,38 @@ createTaskBtn.addEventListener('click', () => {
     });
 
     // subir a tasks y al dom
-    createTask(daysToRepeatTask.join(", "), inputName, inputHour, id);
-    daysToRepeatTask.length > 1 ? daysToRepeatTask.forEach(day => addTaskToDOM(day, inputName, id, inputHour)) : addTaskToDOM(inputDay, inputName, id, inputHour);
+    createTask(daysToRepeatTask, inputName, inputHour, id);
+
+    // mostrar toast 
+    Swal.fire({
+        text: `Se agregó ${inputName} los días ${daysToRepeatTask.join(', ')}`,
+        toast: true,
+        position: 'top-end',
+        timer: 3000,
+        timerProgressBar: true
+    })
 });
 
+// crea tarea y sube a tasks, actualiza LS
+function createTask (day, name, hour, id){
+    let newTask = new Task (day, name, hour, id)
+    tasks.push(newTask);
+    day.forEach(dayFromArray => addTaskToDOM(dayFromArray, newTask));    
+    refreshLS();
+}
+function asingId () {
+    return tasks.length + 1;
+}
+
 // agrega al HTML + funciones borrar y checkear cuando click en btn
-function addTaskToDOM(day, taskName, taskId, taskHour) {
+function addTaskToDOM(day, {name, hour, id, completed}) {
+    // day, taskName, taskId, hour
     const ul = document.getElementById(`${day}-ul`);
     const li = document.createElement('li');
-    const liId = `${day}-${taskName.trim()}-${taskId}`
+    const liId = `${day}-${name.trim()}-${id}`
     li.setAttribute('id', liId);
     li.innerHTML = `
-        <p style="display:inline;" >${taskName}: </p> <span style="font-weight:400;">${taskHour} hs.</span> 
+        <p style="display:inline;" >${name}: </p> <span style="font-weight:400;">${hour} hs.</span> 
         <div class="btn-container"> 
             <button class="btn-edit" id="edit-${liId}">
                 <i class="fa-regular fa-pen-to-square"></i>
@@ -61,9 +83,11 @@ function addTaskToDOM(day, taskName, taskId, taskHour) {
             </div>`;
     ul.append(li);
     
-    deleteTaskBtnDOM(ul, li, `delete-${liId}`, taskId, day);
+    deleteTaskBtnDOM(ul, li, `delete-${liId}`, id, day);
 
-    completeTaskBtnDOM(li, `completed-${liId}`, `icon-check-${liId}`);
+    completeTaskBtnDOM(li, `completed-${liId}`, `icon-check-${liId}`, id, day);
+
+    completed.includes(day) && ( li.classList.toggle('completed'), document.getElementById(`icon-check-${liId}`).classList.toggle('completed') )
 }
 
 // cuando se presiona btn (btnId), html remueve child del father, saca dayToDelete de task (taskId)
@@ -79,16 +103,11 @@ function deleteTaskBtnDOM(father, child, btnId, taskId, dayToDelete) {
 function deleteDayFromTask (taskId, dayToDelete) {
     let taskToDeleteDay = tasks.find(task => task.id == taskId);
     let taskDays = taskToDeleteDay.day;
-    taskDays = taskDays.filter(day => day != dayToDelete)
+    taskDays = taskDays.filter(day => day != dayToDelete);
     taskToDeleteDay.day = taskDays;
-    
-    if (taskDays.length == 0) {
-        deleteTask(taskId)
-    } else if (taskDays.length > 0) {
-        deleteTask(taskId);
-        console.log(`SOLO DEL DÍA ${dayToDelete}`);
-        tasks.push(taskToDeleteDay);
-    }
+    console.log(taskToDeleteDay);
+
+    taskDays.length==0 ? deleteTask(taskId) : ( deleteTask(taskId), tasks.push(taskToDeleteDay) );
     refreshLS();
 }
 
@@ -100,29 +119,23 @@ function deleteTask (taskId) {
     console.log(deletedTask);
 }
 
-// TODO:
+// TODO: cambiar estado para mantener verde cuando se recargue página
 // cuando presiono btn (btnId), se muestra verde li e icon (iconId)
-function completeTaskBtnDOM (li, btnId, iconId){
+function completeTaskBtnDOM (li, btnId, iconId, taskId, dayToComplete){
     const checkBtn = document.getElementById(btnId);
     const checkIcon = document.getElementById(iconId);
+    const completedTask = tasks.find(task => task.id == taskId);
     
     checkBtn.addEventListener('click', ()=> {
         li.classList.toggle('completed');
         checkIcon.classList.toggle('completed');
+        // sumar día a la propiedad completed
+        completedTask.completed.find(completedDays => completedDays == dayToComplete) == null ?
+        completedTask.completed.push(dayToComplete)
+        : completedTask.completed = completedTask.completed.filter(completedDays => completedDays !== dayToComplete);
+        refreshLS()
     })
 }
-
-// crea tarea y sube a tasks, actualiza LS
-function createTask (day, name, hour, id){
-    let newTask = new Task (day, name, hour, id)
-    tasks.push(newTask);
-    refreshLS();
-}
-function asingId () {
-    return tasks.length + 1;
-}
-
-
 
 
 
@@ -130,45 +143,34 @@ function asingId () {
 // despliega modal con confirmacion, elimina todas tareas, actualiza LS
 const deleteAllTasksBtn = document.getElementById('delete-all');
 deleteAllTasksBtn.addEventListener('click', ()=>{
+    Swal.fire({
+        title: 'Advertencia',
+        icon: 'warning',
+        text: 'Borrarás todas las tareas y no podrás recuperarlas.',
+        color: '#111',
+        confirmButtonColor: 'rgb(199, 28, 28)',
+        confirmButtonText: 'BORRAR',
+        showCancelButton: true,
+        cancelButtonText: 'CANCELAR',
 
-    const modal = document.createElement('div');
-    modal.classList.add('modal-container');
-    modal.innerHTML= `
-        <div class="modal" id="modal">
-            <h4 class="day-title">Advertencia</h4>
-            <p>Borarrás todas las tareas y no podrás recuperarlas</p><br>
-            <h5>¿Estás seguro?</h5>
-            <div class="btn-container-modal">
-                <button class="delete-all-confirm" id="delete-all-confirm">Borrar</button>
-                <button class="delete-all-cancel" id="delete-all-cancel">Cancelar</button>
-            </div>
-        </div>
-    `;
-    const body = document.querySelector('body');
-    body.classList.add('no-scroll');
-    body.appendChild(modal);
-
-    const deleteAllConfirm = document.getElementById('delete-all-confirm');
-    deleteAllConfirm.addEventListener('click', ()=>{
-        const allUlSelector = document.querySelectorAll('.day-ul');
-        allUlSelector.forEach(ul => ul.innerHTML = null);
-        tasks = [];
-        refreshLS();
-        removeModal(modal, body)
+        returnFocus: false,
+        focusConfirm: false,
+        focusCancel: false,
+    }).then((result) => {
+        result.isConfirmed && (
+            document.querySelectorAll('.day-ul').forEach(ul => ul.innerHTML = null),
+            tasks = [],
+            refreshLS(),
+            Swal.fire({
+                text:'Se han eliminado todas las tareas',
+                color: '#333',
+                showConfirmButton: false,
+                showCancelButton: true,
+                cancelButtonText: 'OK'
+            })
+        )
     })
-
-    const deleteAllCancel = document.getElementById('delete-all-cancel');
-    deleteAllCancel.addEventListener('click', ()=>{
-        // FIXME:
-        document.getElementById('modal').classList.add('goUp');
-        removeModal(modal, body);
-    })
-
 })
-function removeModal (modalName, modalFather) {
-    modalFather.removeChild(modalName);
-    modalFather.classList.remove('no-scroll');
-}
 
 
 
@@ -176,9 +178,6 @@ function removeModal (modalName, modalFather) {
 
 
 // LOCALSTORAGE
-// if (localStorage.length == 0) { //inicializa
-//     addToLS(tasks, 'allTasks');
-// }
 localStorage.length == 0 && addToLS(tasks, 'allTasks');
 // agrega array a LS
 function addToLS (array, localStorageName) {
@@ -194,10 +193,10 @@ function refreshLS() { //borra LS y agrega tasks
 // imprime en LS en dom cuando carga página
 const allTasksLS = localStorage.getItem('allTasks');
 tasks = [];
-const allTasksLSArray = JSON.parse(allTasksLS);
-tasks = allTasksLSArray;
-allTasksLSArray.forEach(task => {
-    task.day.forEach(element => {
-        addTaskToDOM(element, task.name, task.id, task.hour);
+const allTasksJSON = JSON.parse(allTasksLS);
+tasks = allTasksJSON;
+allTasksJSON.forEach(task => {
+    task.day.forEach(dayToPrint => {
+        addTaskToDOM(dayToPrint, task);
     })
-});
+})
